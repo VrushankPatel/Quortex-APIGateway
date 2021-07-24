57fb2ed0fbbcea8cd78e1c474251a75048623efa
Vrushank Patel <vrushankpatel5@gmail.com>
2021-07-24 23:46:16 +0530

    added quortex-server url selector

--- ReverseProxyAPIGateway.js
+++ ReverseProxyAPIGateway.js
@@ -14,9 +14,8 @@ app.use(bodyParser.json());
 const debugMode = true;
 
 const getUrl = () => {
-	// let date = Date().split(" ")[2];
-	// return date < 15 ? serviceUrl : serviceUrl2;
-	return serviceUrl2;
+	const currentDate = new Date().getDate();
+	return currentDate <= 15 ? serviceUrl : serviceUrl2;
 }
 
 app.post("/api/*", async (request, response) => {
